4f2a10a35d9712e3917cf867148a07c585f40482
Apache mumble <www-data@ip-172-31-14-9.us-west-2.compute.internal>
2014-05-20 08:47:25 +0000

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,49 +1,41 @@
 <?php
-	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
-	 */
+  /**
+   * GIT DEPLOYMENT SCRIPT
+   *
+   * Used for automatically deploying websites via github or bitbucket, more deets here:
+   *
+   *    https://gist.github.com/1809044
+   */
 
-	// The commands
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
+  // The commands
+  $commands = array(
+    'echo $PWD',
+    'whoami',
+    'git pull',
+    'git status',
+    'git submodule sync',
+    'git submodule update',
+    'git submodule status',
+  );
 
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
-
-	// Make it pretty for manual user access (and why not?)
+  // Run the commands for output
+  $output = '';
+  foreach($commands AS $command){
+    // Run it
+    $tmp = shell_exec($command);
+    // Output
+    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+    $output .= htmlentities(trim($tmp)) . "\n";
+  }
 ?>
 <!DOCTYPE HTML>
-<html lang="en-US">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
+  <title>Git deployment</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
-
+Git Deployment Script v0.1
 <?php echo $output; ?>
 </pre>
 </body>
