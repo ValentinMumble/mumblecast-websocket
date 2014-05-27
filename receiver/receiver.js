c4562d2d4faf84a2df6247da5202d5899213e4bc
Valentin Dijkstra <vdijkstra@capptain.com>
2014-05-27 22:49:42 +0200

    Restore good receiver

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -1,39 +1,45 @@
 $(document).ready(function() {
 
   /* Constants. */
+
   var SOCKET_HOST = "54.187.163.215";
   var SOCKET_PORT = 3000;
-  var DEFAULT_ARTWORK_URL = "images/default_artwork.png";
+  var SOUNDCLOUD_API_KEY = "d07779451ce9508678bdd995685ad9b0";
+  var GOOGLE_API_KEY = "AIzaSyBAxvq0xzHA0xEV7cxDsnynFmZFlEMpXBE";
+  var DEFAULT_ARTWORK_URLS = [
+    "images/default_artwork_blue.png",
+    "images/default_artwork_green.png",
+    "images/default_artwork_grey.png",
+    "images/default_artwork_orange.png",
+    "images/default_artwork_purple.png",
+    "images/default_artwork_red.png",
+    "images/default_artwork_yellue.png"
+  ];
 
-  /* Cast */
+  /* ------ Cast { ------ */
 
   var initializeCastApi = function() {
     cast.receiver.logger.setLevelValue(0);
     window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
     console.log('Starting Receiver Manager');
     
-    // handler for the 'ready' event
     castReceiverManager.onReady = function(event) {
       console.log('Received Ready event: ' + JSON.stringify(event.data));
       window.castReceiverManager.setApplicationState("Application status is ready...");
     };
     
-    // handler for 'senderconnected' event
     castReceiverManager.onSenderConnected = function(event) {
       console.log('Received Sender Connected event: ' + event.data);
       console.log(window.castReceiverManager.getSender(event.data).userAgent);
-      userConnected(window.castReceiverManager.getSender(event.data).userAgent);
     };
     
-    // handler for 'senderdisconnected' event
     castReceiverManager.onSenderDisconnected = function(event) {
       console.log('Received Sender Disconnected event: ' + event.data);
       if (window.castReceiverManager.getSenders().length == 0) {
-        window.close();
+        //window.close();
       }
     };
     
-    // handler for 'systemvolumechanged' event
     castReceiverManager.onSystemVolumeChanged = function(event) {
       console.log('Received System Volume Changed event: ' + event.data['level'] + ' ' + event.data['muted']);
     };
@@ -41,7 +47,6 @@ $(document).ready(function() {
     // create a CastMessageBus to handle messages for a custom namespace
     window.messageBus = window.castReceiverManager.getCastMessageBus('urn:x-cast:com.mumble.mumblecast');
 
-    // handler for the CastMessageBus message event
     window.messageBus.onMessage = function(event) {
       console.log('Message [' + event.senderId + ']: ' + event.data);
       // display the message from the sender
@@ -57,55 +62,160 @@ $(document).ready(function() {
     console.log('Receiver Manager started');
   };
 
-  var tracks = [];
-  var index = -1;
-  var currentSound = null;
-
   /* Handle the data coming from the cast manager. */
   var handleData = function(data) {
     console.log(data);
   };
 
+  /* ------ } Cast ------ */
+
+  var tracks = [];
+  var current = {index: -1, trackObject: null, sound: null, paused: true};
+  var youtubePlayer = null;
+
+  var getRandomDefaultArtworkUrl = function() {
+    return DEFAULT_ARTWORK_URLS[Math.floor(Math.random() * DEFAULT_ARTWORK_URLS.length)];
+  };
+
+  var millisecondsToHms = function(d) {
+    d = Number(d) / 1000;
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return ((h > 0 ? h + ":" : "") + (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:") + (s < 10 ? "0" : "") + s);
+  };
+
   var indexOfTrack = function(id) {
     for (var i = 0; i < tracks.length; i++) {
-      if (tracks[i].id == id) {
-        return i;
-      }
+      if (tracks[i].id == id) return i;
     }
-    return false;
+    return -1;
   };
 
   var deleteTrack = function(id) {
     var i = indexOfTrack(id);
     if (i) tracks.splice(i, 1);
+    return i;
   };
 
   var loadTrack = function(trackObject) {
     tracks.push(trackObject);
   };
 
+  var clearPlayers = function() {
+    /* Stop soundManager2 & destruct sound if any. */
+    soundManager.stopAll();
+    if (current.sound != null) {
+      current.sound.destruct();
+      current.sound = null;
+    }
+    /* Stop YouTube playing & loading. */
+    youtubePlayer.stopVideo();
+  };
+
   var playTrack = function(trackObject) {
-    if (trackObject) {
-      SC.stream("/tracks/" + trackObject.trackId, function(sound) {
-        if (currentSound != null) currentSound.stop();
-        currentSound = sound;
-        currentSound.play();
-      });
-      displayTrack(trackObject);
+    $(".disclaimer").fadeOut();
+    clearPlayers();
+    current.trackObject = trackObject;
+    if (trackObject.provider == "soundcloud") {
+      playSoundCloudTrack(trackObject);
+    } else if (trackObject.provider == "youtube") {
+      playYouTubeTrack(trackObject);
+    }
+    socket.emit("track playing", trackObject.id);
+    current.paused = false;
+    socket.emit("paused", current.paused);
+  };
+
+  var playSoundCloudTrack = function(trackObject) {
+    $(".player").not("#soundcloudPlayer").hide();
+    $(".cover .overlay").removeClass("paused");
+    var $soundcloudPlayer = $("#soundcloudPlayer").addClass("hidden").show();
+    var $elapsed = $soundcloudPlayer.find(".elapsed").text("");
+    var $remaining = $soundcloudPlayer.find(".remaining").text("");
+    var $comment = $soundcloudPlayer.find(".comment").text("");
+    var artworkUrl = trackObject.artworkUrl == null ? getRandomDefaultArtworkUrl() : trackObject.artworkUrl.replace("large", "t300x300");
+    var $artwork = $soundcloudPlayer.find(".artwork");
+    if ($artwork.attr("src") != artworkUrl) {
+      $artwork.addClass("hidden").attr("src", artworkUrl).load(function() { $artwork.removeClass("hidden"); });
+    }
+    $soundcloudPlayer.find(".title").text(trackObject.title);
+    $soundcloudPlayer.find(".artist").text(trackObject.artist);
+    $soundcloudPlayer.removeClass("hidden");
+
+    var defaultColor = "#222";
+    var loadedColor = "#2A2A2A";
+    var playedColor = "#ff6600";
+
+    $("#waveform").empty();
+    var waveform = new Waveform({
+      container: $("#waveform")[0],
+      innerColor: defaultColor
+    });
+
+    waveform.dataFromSoundCloudTrack({waveform_url: trackObject.waveformUrl});
+    var options = waveform.optionsForSyncedStream({
+      defaultColor: defaultColor,
+      loadedColor: loadedColor,
+      playedColor: playedColor
+    });
+
+    options.onfinish = playNext;
+    var wp = options.whileplaying;
+    options.whileplaying = function() {
+      wp();
+      $elapsed.text(millisecondsToHms(this.position));
+      // +1 because of soundManager2 HTML5 bug
+      $remaining.text("-" + millisecondsToHms(this.duration - this.position + 1));
+    };
+    options.ontimedcomments = function(comments) {
+      $comment.text(comments[0].user.username + ": " + comments[0].body);
+    };
+
+    SC.stream("/tracks/" + trackObject.providerId, options, function(sound){
+      /* Prevention in case this callback is late and a sound is already playing. */
+      if (current.sound == null) {
+        current.sound = sound;
+        current.sound.play();
+      }
+    });
+  };
+
+  var playYouTubeTrack = function(trackObject) {
+    $(".player").not("#youtubePlayer").hide();
+    $("#youtubePlayer").addClass("hidden").show();
+    youtubePlayer.loadVideoById(trackObject.providerId);
+    $("#youtubePlayer").removeClass("hidden");
+  };
+
+  var pauseTrack = function() {
+    if (current.trackObject != null) {
+      if (current.trackObject.provider == "soundcloud") {
+        current.sound.togglePause();
+        $(".cover .overlay").toggleClass("paused");
+      } else if (current.trackObject.provider == "youtube") {
+        current.paused ? youtubePlayer.playVideo() : youtubePlayer.pauseVideo();
+      }
+      current.paused = !current.paused;
+      socket.emit("paused", current.paused);
     }
   };
 
-  var displayTrack = function(trackObject) {
-    var $currentTrack = $("#currentTrack").fadeOut();
-    SC.get("/tracks/" + trackObject.trackId, function(track, error){
-      var artworkUrl = track.artwork_url == null ? DEFAULT_ARTWORK_URL : track.artwork_url.replace("large", "crop");
-      $currentTrack.find(".artwork").hide().attr("src", artworkUrl).load(function() { $(this).fadeIn(); });
-      $currentTrack.find(".title").text(track.title);
-      $currentTrack.find(".user").text(track.user.username);
-      $currentTrack.stop().fadeIn();
-      console.log("playing track: " + track.title);
-    });
+  var playNext = function() {
+    if (current.index < tracks.length - 1) {
+      current.index++;
+      playTrack(tracks[current.index]);
+    }
   };
+
+  var playPrevious = function() {
+    if (current.index > 0) {
+      current.index--;
+      playTrack(tracks[current.index]);
+    }
+  };
+
+  /* ------ Socket { ------ */
 
   /* Connection to the node/websockets server. */
   var socket = io.connect("http://" + SOCKET_HOST + ":" + SOCKET_PORT);
@@ -115,33 +225,60 @@ $(document).ready(function() {
     for (var i = 0; i < data.length; i++) {
       loadTrack(data[i]);
     }
-    //playTrack(tracks[0]);
+
+    /* Initialize YouTube player. */
+    youtubePlayer = new YT.Player('youtubePlaceholder', {
+      playerVars: {
+        autoplay: 1,
+        controls: 0
+      },
+      events: {
+        onStateChange: function(event) {
+          if (event.data === 0) playNext();
+        }
+      }
+    });
   });
 
-  socket.on("new track", function(trackObject) {
+  /* When the server tells to add this track. */
+  socket.on("track added", function(trackObject) {
     loadTrack(trackObject);
   });
 
-  socket.on("delete track", function(id) {
-    deleteTrack(id);
+  /* When the server tells to delete this track. */
+  socket.on("track deleted", function(id) {
+    var deletedId = deleteTrack(id);
+    if (deletedId <= current.index) current.index--;
   });
 
+  /* When the server tells to play this track. */
   socket.on("play track", function(id) {
-    playTrack(tracks[indexOfTrack(id)]);
+    current.index = indexOfTrack(id);
+    playTrack(tracks[current.index]);
   });
 
-  /* Main */
+  /* When the server tells to pause the current track. */
+  socket.on("pause", pauseTrack);
 
-  window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
-    if (loaded) {
-      initializeCastApi();
-    } else {
-      console.log(errorInfo);
-    }
-  };
+  /* When the server tells to play the next track. */
+  socket.on("next", playNext);
+
+  /* When the server tells to play the previous track. */
+  socket.on("previous", playPrevious);
+
+  /* ------ } Socket ------ */
+
+  /* ------ Main ------ */
+
+  initializeCastApi();
 
   SC.initialize({
-    client_id: "d07779451ce9508678bdd995685ad9b0"
+    client_id: SOUNDCLOUD_API_KEY
+  });
+
+  soundManager.setup({
+    useHTML5Audio: true,
+    preferFlash: false,
   });
 
   socket.emit("i am receiver");
